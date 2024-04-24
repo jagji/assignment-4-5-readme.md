eeee5a66fedc59e582d508aecfbbe1ce75120ddc
jagjit kaur <155929599+jagji@users.noreply.github.com>
2024-04-24 12:11:53 -0400

    Create artist.js

---
+++ artist.js
@@ -0,0 +1,40 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.categories = [
+  {
+    id: "cat-1",
+    name: "Googoosh",
+    links: [
+      { url: "https://en.wikipedia.org/wiki/Googoosh", name: "Wikipedia" },
+      { url: "https://googooshlive.com/", name: "Home page" }
+    ]
+  },
+  {
+    id: "cat-2",
+    name: "Taylor Swift",
+    links: [
+      { url: "https://en.wikipedia.org/wiki/Taylor_Swift", name: "Wikipedia" },
+      { url: "https://taylorswift.com/", name: "Home page" }
+    ]
+  },
+  {
+    id: "cat-3",
+    name: "Arctic Monkeys",
+    links: [
+      { url: "https://en.wikipedia.org/wiki/Arctic_Monkeys", name: "Wikipedia" },
+      { url: "https://www.arcticmonkeys.com/", name: "Home page" }
+    ]
+  },
+  {
+    id: "cat-4",
+    name: "Ruel",
+    links: [
+      { url: "https://en.wikipedia.org/wiki/Ruel", name: "Wikipedia" },
+      { url: "https://www.oneruel.com/", name: "Home page" }
+    ]
+  }
+];
