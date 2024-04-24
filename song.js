eeaca79fe51c1fbc75d24497a75323fc22faa271
jagjit kaur <155929599+jagji@users.noreply.github.com>
2024-04-24 12:12:34 -0400

    Create song.js

---
+++ song.js
@@ -0,0 +1,287 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    id: "RS21",
+    title: "Evermore",
+    description: "Artist: Taylor Swift, Release: Sep 20, 2012, Available in: YouTube",
+    year: 2012,
+    Duration: "4:14",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/RsEZmictANA?si=tZl0u6q68wKjRm-N"
+  },
+  {
+    id: "RS22",
+    title: "Nagoo Bedroud",
+    description: "Artist: Googoosh, Release: Jul 24 2020, Available in: Vinyl",
+    year: 2012,
+    Duration: "03:15",
+    discontinued: false,
+    categories: ["cat-2"],
+    youtubeLink: "https://youtu.be/xVjZqa0Udu8?si=WP9DYN9stn6tMBGO"
+  },
+  {
+    id: "RS23",
+    title: "Heiress",
+    description: "Artist: Novo Amor and Ed Tullett, Release: Nov 10 2017, Available in: Vinyl",
+    year: 2017,
+    Duration: "04:15",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/RdueQoNpiSE?si=MSzNivWgXAuepxbv"
+  },
+  {
+    id: "RS24",
+    title: "Derakht",
+    description: "Artist: Ebi, Release: Jul 29, 2010, Available in: YouTube",
+    year: 2010,
+    Duration: "03:76",
+    discontinued: false,
+    categories: ["cat-2"],
+    youtubeLink: "https://youtu.be/uSV9hkcOcgA?si=LhzcinIkrm4ioJXi"
+  },
+  {
+    id: "RS25",
+    title: "Red (Taylor's Version)",
+    description: "Artist: Taylor Swift, Release: Nov 12 2021, Available in: Vinyl",
+    year: 2021,
+    Duration: "05:15",
+    discontinued: false,
+    categories: ["cat-1"],
+    youtubeLink: "https://youtu.be/R_rUYuFtNO4?si=jzxY56hmfuHzscS6"
+  },
+  {
+    id: "RS26",
+    title: "AM",
+    description: "Artist: Arctic Monkeys, Release: Aug 1 2013, Available in: Vinyl",
+    year: 2013,
+    Duration: "05:05",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/ddwxyI3hgz8?si=wSE7JymKLihMXfk3"
+  },
+  {
+    id: "RS27",
+    title: "Bright Lights, Red Eyes",
+    description: "Artist: Ruel, Release: 2020, Available in: Vinyl",
+    year: 2020,
+    Duration: "03:65",
+    discontinued: false,
+    categories: ["cat-1"],
+    youtubeLink: "https://youtu.be/bhGjjwvSXLE?si=Pq0cgPxLrCeTq3-R"
+  },
+  {
+    id: "RS28",
+    title: "Vide Noir",
+    description: "Artist: Lord Huron, Release: Apr 20 2018, Available in: Vinyl",
+    year: 2018,
+    Duration: "05:15",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/G757Elpf8YI?si=Aru9qjTQHShQ8tDz"
+  },
+  {
+    id: "RS29",
+    title: "Cheshme Man",
+    description: "Artist: Dariush, Release: Sep 25, 2008, Available in: YouTube",
+    year: 2008,
+    Duration: "04:25",
+    discontinued: false,
+    categories: ["cat-2"],
+    youtubeLink: "https://youtu.be/RiekTOPB12g?si=ahIOyAtO0e-rvYxo"
+  },
+  {
+    id: "RS30",
+    title: "Fine Line",
+    description: "Artist: Harry Styles, Release: Dec 13 2019, Available in: Vinyl",
+    year: 2019,
+    Duration: "04:10",
+    discontinued: false,
+    categories: ["cat-1"],
+    youtubeLink: "https://youtu.be/Ojp71GGm-LQ?si=PsDSm0_FMOvXoQ1M"
+  },
+  {
+    id: "RS31",
+    title: "Strange Trails",
+    description: "Artist: Lord Huron, Release: Apr 7 2015, Available in: Vinyl",
+    year: 2015,
+    Duration: "04:00",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/KtlgYxa6BMU?si=DBZhjYxQdfzyKbrE"
+  },
+  {
+    id: "RS32",
+    title: "Favourite Worst Nightmare",
+    description: "Artist: Arctic Monkeys, Release: Apr 18 2007, Available in: Vinyl",
+    year: 2007,
+    Duration: "02:30",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/30w8DyEJ__0?si=XrCrsgtqpjne1LcY"
+  },
+  {
+    id: "RS33",
+    title: "Mara Bebakhsh",
+    description: "Artist: Alireza Ghorbani, Release: Aug 23, 2023, Available in: YouTube",
+    year: 2023,
+    Duration: "05:15",
+    discontinued: false,
+    categories: ["cat-2"],
+    youtubeLink: "https://youtu.be/u5g5JeGq3kM?si=u2siog-jkszVJoxu"
+  },
+  {
+    id: "RS34",
+    title: "Social Cues",
+    description: "Artist: Cage the Elephant, Release: Apr 19 2019, Available in: Vinyl",
+    year: 2019,
+    Duration: "03:00",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/6GUzsinBhzY?si=sSkxh3RIw_U1QZTr"
+  },
+  {
+    id: "RS35",
+    title: "Paradise EP",
+    description: "Artist: Chase Atlantic, Release: 2016, Available in: CD",
+    year: 2016,
+    Duration: "04:12",
+    discontinued: true,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/E_zXy3ZOMTM?si=Hg4kVxjgV6xDckc1"
+  },
+  {
+    id: "RS36",
+    title: "Born to Die",
+    description: "Artist: Lana Del Rey, Release: 2017, Available in: Vinyl",
+    Duration: "03:15",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "Born https://youtu.be/pYSjawMuZE4?si=oJTXsMjS-gbY8BJJ Die"
+  },
+  {
+    id: "RS37",
+    title: "Entekhab",
+    description: "Artist: Shadmehr, Release:  Jul 11, 2012, Available in: Youtube",
+    year: 2012,
+    Duration: "03:55",
+    discontinued: true,
+    categories: ["cat-2"],
+    youtubeLink: "https://youtu.be/T3pBOfMjQIw?si=NgclZyyO-J9x7XoE"
+  },
+  {
+    id: "RS38",
+    title: "Dreamland",
+    description: "Artist: Glass Animals, Release: 7 Aug 2020, Available in: Vinyl",
+    year: 2020,
+    Duration: "01:15",
+    discontinued: false,
+    categories: ["cat-1"],
+    youtubeLink: "https://youtu.be/mqjo1ZrvaBs?si=skKmvrfs8RmSiGpW"
+  },
+  {
+    id: "RS39",
+    title: "NFR",
+    description: "Artist: Lana Del Rey, Release: 2017, Available in: Vinyl",
+    year: 2017,
+    Duration: "03:05",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/GpEEApTj7hw?si=2Fybs4AKYfbVlQOv"
+  },
+  {
+    videoId: "sR9d5doziDs",
+    title: "The Slow Rush",
+    description: "Artist: Tame Impala, Release: Feb 14 2020, Available in: Vinyl",
+    year: 2020,
+    Duration: "05:25",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/sR9d5doziDs?si=dGhKhE8ltwOcChan"
+  },
+  {
+    id: "RS41",
+    title: "Ultraviolence",
+    description: "Artist: Lana Del Rey, Release: Jun 13 2014, Available in: Vinyl",
+    year: 2014,
+    Duration: "04:10",
+    discontinued: false,
+    categories: ["cat-3"],
+    youtubeLink: "https://youtu.be/EJSk2RySqKg?si=htpURnYUJXGtwG2N"
+  },
+  {
+    id: "RS42",
+    title: "Superache",
+    description: "Artist: Conan Gray, Release: Jun 24 2022, Available in: Vinyl",
+    year: 2022,
+    Duration: "03:15",
+    discontinued: false,
+    categories: ["cat-1"],
+    youtubeLink: "https://youtu.be/DDqOI71Aq_M?si=Wtr0TLL1fnFh0DjH"
+  },
+  {
+    id: "RS43",
+    title: "Friends EP",
+    description: "Artist: Chase Atlantic, Release: Feb 21 2017, Available in: CD",
+    year: 2017,
+    Duration: "04:00",
+    discontinued: false,
+    categories: ["cat-1"],
+    youtubeLink: "https://youtu.be/qMHkWgOE6X8?si=zttvRINOTLN5inJa"
+  },
+  {
+    id: "RS44",
+    title: "Ctrl",
+    description: "Artist: SZA, Release: 2017, Available in: CD",
+    year: 2017,
+    Duration: "02:15",
+    discontinued: false,
+    categories: ["cat-4"],
+    youtubeLink: "https://youtu.be/Z-T_O_vl-8Y?si=s-hIhzyJCULZBZjj"
+  },
+  {
+    id: "RS45",
+    title: "After Hours",
+    description: "Artist: The Weeknd, Release: March 20 2020, Available in: Vinyl",
+    year: 2020,
+    Duration: "03:45",
+    discontinued: false,
+    categories: ["cat-4"],
+    youtubeLink: "https://youtu.be/ygTZZpVkmKg?si=zwg6wVOwSE2hopvF"
+  },
+  {
+    id: "RS46",
+    title: "Dawn FM",
+    description: "Artist: The Weeknd, Release: Feb Jan 7 2022, Available in: Vinyl",
+    year: 2022,
+    Duration: "05:15",
+    discontinued: false,
+    categories: ["cat-4"],
+    youtubeLink: "https://youtu.be/w8eFZoOcYKc?si=I6m3NjYWblk50Fiu"
+  },
+  {
+    id: "RS47",
+    title: "Starboy",
+    description: "Artist: The Weeknd, Release: Nov 25 2016, Available in: Vinyl",
+    year: 2016,
+    Duration: "06:15",
+    discontinued: false,
+    categories: ["cat-4"],
+    youtubeLink: "https://youtu.be/34Na4j8AVgA?si=kl9e2A5ZdFCRlMAh"
+  },
+  {
+    id: "RS48",
+    title: "PARTYNEXTDOOR (EP)",
+    description: "Artist: PARTYNEXTDOOR, Release: Jul 1 2013, Available in: CD",
+    year: 2013,
+    Duration: "03:34",
+    discontinued: false,
+    categories: ["cat-4"],
+    youtubeLink: "https://youtu.be/b24y0hAyiHc?si=LRC_h-KMlvw6BjWZ"
+  }
+];
